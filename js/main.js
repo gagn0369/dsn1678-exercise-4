ee969785b99d617c1c10b48a9a363faef60a4958
gagn0369 <madeleinemagnolia@gmail.com>
2014-10-09 10:46:29 -0400

    all code...oops

---
+++ js/main.js
@@ -0,0 +1,51 @@
+var $win = $(window);
+var $sun = $(".sun");
+var $sunSection = $(".sun-section");
+
+var $dipperSection = $(".dipper-section");
+var $dipper = $(".dipper");
+
+var $shuttleSection = $(".shuttle-section");
+var $shuttle = $(".shuttle");
+
+var $shipSection = $(".ship-section");
+var $ship1 = $(".ship1")
+var $ship2 = $(".ship2")
+var $ship3 = $(".ship3")
+var $ship4 = $(".ship4")
+
+$win.on('scroll', function () {
+	var scrollPos = $win.scrollTop();
+	
+	$sun .css("transform", "rotate(" + scrollPos / 5 + "deg)");
+	$sunSection.css("background-position", "center " + scrollPos / 2 + "px");
+	$shipSection.css("background-position", "center " + scrollPos / 2 + "px");
+});
+
+
+$dipperSection.waypoint(function () {
+	$dipper.addClass('js-dipper-fade');
+}, { offset: '50%' });
+
+$shuttleSection.waypoint(function () {
+	$shuttle.addClass('js-shuttle-fade');
+}, { offset: '50%' });
+
+$shipSection.waypoint(function () {
+	$ship1.addClass('js-ship-fade-1');
+}, { offset: '50%' });
+
+$shipSection.waypoint(function () {
+	$ship2.addClass('js-ship-fade-2');
+}, { offset: '50%' });
+
+$shipSection.waypoint(function () {
+	$ship3.addClass('js-ship-fade-1');
+}, { offset: '50%' });
+
+$shipSection.waypoint(function () {
+	$ship4.addClass('js-ship-fade-1');
+}, { offset: '50%' });
+
+
+
